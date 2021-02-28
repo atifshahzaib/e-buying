5fe2f57af70994d64f0fd3d002a8660f0270484b
developer <dev@AtifShahzaib.local>
2021-02-28 02:23:22 -0800

    relationship defined between models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize: "200x200"
+  end
+  validates :name, :price, :quantity, presence: true
+  scope :available, -> { where("quantity > 0") }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :orders
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } , :uniqueness => { :case_sensitive => false }
 end
