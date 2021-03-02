a7383b22768f9f7454e2cdd45148fcb6046ffb82
developer <dev@AtifShahzaib.local>
2021-03-02 11:32:35 -0800

    relationship defined between product and order item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_one_attached :avatar
+  has_many :order_items, dependent: :nullify
   validates :name, :price, :quantity, presence: true
   scope :available, -> { where("quantity > 0") }
 end
